34a7e8e9373d2beec205fb6119461adcaff89c7e
arabambi <arababs2015@gmail.com>
2022-02-11 10:50:26 +0100

    useContext init

---
+++ R3a/src/ststes/UseContext.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const UseContext = () => {
+
+
+          return (
+                    <div>
+                              UseContext
+                    </div>
+          )
+}
+
+
+export default UseContext
+
+/*
+UseContext:
+React Context is a way to manage state globally.
+It can be used together with the useState Hook to share state 
+between deeply nested components more easily than with 
+useState alone.
+
+
+
+
+
+
+
+
+
+*/ 
--- R3a/src/ststes/UseEffect.jsx
+++ R3a/src/ststes/UseEffect.jsx
@@ -83,11 +83,28 @@ function Counter() {
 
 NB: Clicking d btn causes a re-rendering, wc is what useEffect needs
 
+Effect Cleanup
+Some effects require cleanup to reduce memory leaks.
+Timeouts, subscriptions, event listeners, and other effects that are 
+no longer needed should be disposed.
+We do this by including a return function at the end of the 
+useEffect Hook.
 
+function Timer() {
+          const [count, setCount] = useState(0);
 
+          useEffect(() => {
+                    let timer = setTimeout(() => {
+                              setCount((count) => count + 1);
+                    }, 1000);
 
+                    return () => clearTimeout(timer)
+          }, []);
 
+          return <h1>I've rendered {count} times!</h1>;
+}
 
+Note: To clear the timer, we had to name it.
 
 
           
