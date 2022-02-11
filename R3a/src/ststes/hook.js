bd155348acd4a639263581a50eb503d94c4e3f1e
arabambi <arababs2015@gmail.com>
2022-02-11 10:03:38 +0100

    Hooks started

---
+++ R3a/src/ststes/hook.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+const Hook = () => {
+
+
+          return (
+                    <div>
+                              Hook
+                    </div>
+          )
+}
+
+
+export default Hook
+
+/*
+Hooks allow us to "hook" into React features such as 
+state and lifecycle methods.
+
+You must import Hooks from react.
+Here we are using the useState Hook to keep track of 
+the application state.
+State generally refers to application data or properties 
+that need to be tracked.
+
+
+Hook Rules
+There are 3 rules for hooks:
+Hooks can only be called inside React function components.
+Hooks can only be called at the top level of a component.
+Hooks cannot be conditional
+
+
+*/ 
