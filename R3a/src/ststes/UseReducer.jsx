9d9befb90ada9e8f32905e4f674f94a64d1c8809
arabambi <arababs2015@gmail.com>
2022-02-11 12:42:29 +0100

    useReducer started

---
+++ R3a/src/ststes/UseReducer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+const UseReducer = () => {
+
+
+          return (
+                    <div>
+                              UseReducer
+                    </div>
+          )
+}
+
+
+export default UseReducer
+
+/*
+UseReducer:
+The useReducer Hook is similar to the useState Hook.
+It allows for custom state logic.
+If you find yourself keeping track of multiple pieces of 
+state that rely on complex logic, useReducer may be useful.
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/ 
